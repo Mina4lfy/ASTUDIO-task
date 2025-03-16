c232720b47e7b955f89f02f2a993437b041299b8
Mina Alfy <minaalfy8@gmail.com>
2025-03-16 03:52:06 +0300

    ADD: cron command to  hourly purge revoked and expired passport tokens

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+# Hourly clear expired passport tokens.
+Schedule::command('passport:purge')->hourly();
